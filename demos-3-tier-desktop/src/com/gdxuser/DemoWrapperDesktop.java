52ff19c9712afd09823d192fb17eab13cac147a7
Chris (radioking) <radioking@gmx.de>
2011-08-19 22:52:26 +0200

    Added entry for HelloWorld from libGDX team to start it manually as single test on Desktop

--- demos-3-tier-desktop/src/com/gdxuser/DemoWrapperDesktop.java
+++ demos-3-tier-desktop/src/com/gdxuser/DemoWrapperDesktop.java
@@ -22,19 +22,30 @@ public class DemoWrapperDesktop {
 		// low tech switching method to choose which demo to view! comments!
 
 //		new LwjglApplication(new SimpleTest(),
-//				"LibGDX users DemoWrapper Desktop", 800, 600, false);
+//				"LibGDX users DemoWrapper Desktop: SimpleTest", 800, 600, false);
+		
 //		new LwjglApplication(new DecalWall(),
-//				"LibGDX users DemoWrapper Desktop", 800, 600, false);
+//				"LibGDX users DemoWrapper Desktop: DecalWall", 800, 600, false);
+		
 //		new LwjglApplication(new ModelViewer(),
-//				"LibGDX users DemoWrapper Desktop", 800, 600, false);
+//				"LibGDX users DemoWrapper Desktop: ModelViewer", 800, 600, false);
+		
 //		new LwjglApplication(new Box2DTest(),
-//				"LibGDX users DemoWrapper Desktop", 800, 600, false);
+//				"LibGDX users DemoWrapper Desktop: Box2DTest", 800, 600, false);
+		
 //		new LwjglApplication(new PhysicsTest(),
-//				"LibGDX users DemoWrapper Desktop", 800, 600, false);
+//				"LibGDX users DemoWrapper Desktop: PhysicsTest", 800, 600, false);
+		
 //		new LwjglApplication(new IsoCamTest(),
-//				"LibGDX users DemoWrapper Desktop", 800, 600, false);
+//				"LibGDX users DemoWrapper Desktop: IsoCamTest", 800, 600, false);
+		
+//		new LwjglApplication(new HelloWorld(),
+//				"LibGDX users DemoWrapper Desktop: HelloWorld", 800, 600, false);
+		
 //		new LwjglApplication(new QuadRenderTest(),
 //				"LibGDX users DemoWrapper Desktop: QuadRenderTest", 800, 600, false);
+
+		// This wraps all Tests, navigate through them with '.' and ','
 		new LwjglApplication(new TestCollection(),
 				"LibGDX users DemoWrapper Desktop", 800, 600, false);
 	}
