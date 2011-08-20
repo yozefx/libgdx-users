13550218cdea06c4e015add692edfb31098346ea
Chris (radioking) <radioking@gmx.de>
2011-08-20 03:12:20 +0200

    updated DecalWall to use the new Decal lookAt method Mario provided

--- demos-3-tier-main/src/com/gdxuser/util/DecalSprite.java
+++ demos-3-tier-main/src/com/gdxuser/util/DecalSprite.java
@@ -2,34 +2,29 @@ package com.gdxuser.util;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Camera;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureWrap;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g3d.decals.Decal;
-import com.badlogic.gdx.graphics.g3d.decals.DecalBatch;
-import com.badlogic.gdx.math.Vector3;
 
-public class DecalSprite{
+public class DecalSprite {
 
 	public Decal sprite;
 
 	public DecalSprite() {
 		// constructor
 	}
-	
+
 	public DecalSprite build(String imgPath) {
-		TextureWrap texWrap = Texture.TextureWrap.ClampToEdge;	// default
+		TextureWrap texWrap = Texture.TextureWrap.ClampToEdge; // default
 		return build(imgPath, texWrap);
 	}
 
 	public DecalSprite build(String imgPath, TextureWrap texWrap) {
-		Texture image = new Texture(
-				Gdx.files.internal(imgPath));
+		Texture image = new Texture(Gdx.files.internal(imgPath));
 		image.setFilter(Texture.TextureFilter.Linear,
 				Texture.TextureFilter.Linear);
-		image.setWrap(Texture.TextureWrap.Repeat,
-				Texture.TextureWrap.Repeat);
+		image.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
 
 		float w = image.getWidth();
 		float h = image.getHeight();
@@ -37,12 +32,8 @@ public class DecalSprite{
 		return this;
 	}
 
-	// This method has become obsolete as of implementation in libGDX
-	// having the decal lookAt the camera...
 	public void faceCamera(Camera oCam) {
-		//TODO: change to according libGDX signature/method call
-		Vector3 dir = new Vector3(oCam.position.cpy().sub(sprite.getPosition()).nor());
-		sprite.setRotation(dir, oCam.up.cpy().nor());
+		sprite.lookAt(oCam.position.cpy(), oCam.up.cpy().nor());
 	}
 
 	public void update(float delta) {
