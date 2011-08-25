ac3dce5c4e8309ba9ff9e9a144407837cb04a17e
Chris (radioking) <radioking@gmx.de>
2011-08-25 12:03:17 +0200

    added getters.\n added default orientation.\n added some comments.

--- demos-1-tier-android/src/com/wiztoybox/examplelauncher/Demo.java
+++ demos-1-tier-android/src/com/wiztoybox/examplelauncher/Demo.java
@@ -1,5 +1,7 @@
 package com.wiztoybox.examplelauncher;
 
+import android.content.pm.ActivityInfo;
+
 /* Data structure for examples list */
 public class Demo
 {
@@ -11,12 +13,29 @@ public class Demo
 	{
 		this.title = title;
 		this.classname = classname;
+		// adds default orientation ...
+		this.orientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED;
 	}
 
+	// classname has to end with ".class"
+	// orientation can be ORIENTATION_PORTRAIT
+	// or ORIENTATION_LANDSCAPE or ORIENTATION_SQUARE
 	public Demo(String title, Class<?> classname, int orientation)
 	{
 		this.title = title;
 		this.classname = classname;
 		this.orientation = orientation;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public Class<?> getClassname() {
+		return classname;
+	}
+
+	public int getOrientation() {
+		return orientation;
 	}
 }	
